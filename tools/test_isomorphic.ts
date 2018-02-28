0a0240c7a755d458cdb35b928a5e6b8918ab469f
Ryan Dahl <ry@tinyclouds.org>
2018-02-27 19:46:51 -0500

    Move test_* files to tools.
    - Include gendoc_test in test (it wasn't being run before).
    - Move the binding_test invoction from tools/test.js to
      tools/test_node.ts.

---
+++ tools/test_isomorphic.ts
@@ -0,0 +1,9 @@
+import "../src/api_test";
+import "../src/backend_test";
+import "../src/dataset_test";
+import "../src/format_test";
+import "../src/mnist_test";
+import "../src/nn_example_test";
+import "../src/npy_test";
+import "../src/params_test";
+import "../src/util_test";
